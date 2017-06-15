73cbcea7d2702fe38eb72df5f1a5c96b0b767a0d
Gleb <mail@g1eb.com>
2017-06-15 16:04:40 +0200

    Added interactivity to the chef component.

--- src/components/Chef.js
+++ src/components/Chef.js
@@ -3,13 +3,42 @@ import React from 'react'
 import '../styles/chef.css'
 
 class Chef extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      'animate': false,
+    }
+  }
+
+  getView() {
+    if ( this.props.isCooking ) {
+      return <h1>Animation</h1>
+    } else {
+      return (
+        <div className='container'>
+          <h1>chef</h1>
+          <div className='btn' onClick={this.props.cook}>
+            Play
+          </div>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className='chef'>
-        <h1>chef</h1>
+        {this.getView()}
       </div>
     )
   }
+}
+
+Chef.defaultProps = {
+  isCooking: false,
+  cook: undefined,
 }
 
 export default Chef
