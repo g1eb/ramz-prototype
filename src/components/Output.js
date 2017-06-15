8dea6ece0c2db529649700d7f690b3acb3c33d62
Gleb <mail@g1eb.com>
2017-06-15 13:41:06 +0200

    Added initial output component.

---
+++ src/components/Output.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/output.css'
+
+class Output extends React.Component {
+  render() {
+    return (
+      <div className='output'>
+        <h1>output component</h1>
+      </div>
+    )
+  }
+}
+
+export default Output
