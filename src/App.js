6a0483b232279070c5cffce7d373e509fe3e0193
Gleb <mail@g1eb.com>
2017-06-15 15:04:40 +0200

    Make users agree to the terms before proceding to the next state.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import Background from './components/Background'
+import Agreement from './components/Agreement'
 import Output from './components/Output'
 import Input from './components/Input'
+import Chef from './components/Chef'
 
 import './styles/app.css'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      agreement: false,
+    }
+  }
+
+  agree() {
+    this.setState({
+      'agreement': true,
+    })
+  }
+
+  getView() {
+    if ( this.state.agreement ) {
+      return (
+        <div>
+          <Chef />
+          <Input />
+          <Output />
+        </div>
+      )
+    } else {
+      return <Agreement onAgree={this.agree.bind(this)} />
+    }
+  }
+
   render() {
     return (
       <div>
-        <Input />
-        <Output />
+        {this.getView()}
         <Background />
       </div>
     );
