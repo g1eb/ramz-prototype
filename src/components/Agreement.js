e11b860d0cc7dcb4850a7b70a6d4fd9dadbfa04c
Gleb <mail@g1eb.com>
2017-06-15 14:27:12 +0200

    Added initial agreement component.

---
+++ src/components/Agreement.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/agreement.css'
+
+class Agreement extends React.Component {
+  render() {
+    return (
+      <div className='agreement'>
+        <h1>Agreement component</h1>
+      </div>
+    )
+  }
+}
+
+export default Agreement
