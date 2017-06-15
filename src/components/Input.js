192fbcacbeaf3694e92e0a7a48bccae3f1bdf869
Gleb <mail@g1eb.com>
2017-06-15 13:47:37 +0200

    Added initial input component.

---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/input.css'
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div className='input'>
+        <h1>input component</h1>
+      </div>
+    )
+  }
+}
+
+export default Input
