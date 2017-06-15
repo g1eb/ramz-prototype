7b730d8ecd08745d27c550091f1b22eea1b6b72c
Gleb <mail@g1eb.com>
2017-06-15 12:28:49 +0200

    Render backgorund component inside the app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import Background from './components/Background'
 import './styles/app.css'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Background />
     );
   }
 }
