7b8ada8ae872dae54843c2d9f63107b9804e137f
Gleb <mail@g1eb.com>
2017-06-15 13:57:55 +0200

    Added initial component for the chef.

---
+++ src/components/Chef.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/chef.css'
+
+class Chef extends React.Component {
+  render() {
+    return (
+      <div className='chef'>
+        <h1>chef</h1>
+      </div>
+    )
+  }
+}
+
+export default Chef
